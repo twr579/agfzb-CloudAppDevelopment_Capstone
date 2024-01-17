e1d5acf96be7fd4ac35e7b6034b029f92862f987
twr579 <twreynolds98@gmail.com>
2024-01-17 14:15:02 -0500

    Add dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -27,7 +27,10 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path(route='dealer_details/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review/<int:dealer_id>', view=views.add_review, name='add_review'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -100,9 +100,17 @@ def get_dealerships(request):
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == "POST":
+        return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
 
